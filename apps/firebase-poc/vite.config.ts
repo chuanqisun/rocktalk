e06a04558a02ec46629ba50d504be6dbfd944880
Chuanqi Sun <chuanqi.sun.dev@gmail.com>
2026-05-20 19:21:45 -0400

    fix deployment

--- apps/firebase-poc/vite.config.ts
+++ apps/firebase-poc/vite.config.ts
@@ -2,7 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/rock-talk/",
+  base: "/rocktalk/",
   build: {
     rollupOptions: {
       input: {
